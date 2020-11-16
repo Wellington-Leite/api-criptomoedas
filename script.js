15fd157f2dd5770c7d9a82ffbe739032c569383a
Wellington Leite <wellingtonleite15@gmail.com>
2020-11-16 00:19:13 -0300

    funcao-buscar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //My api key
 var apiKey = {
-  key: "Sua chave aqui",
+  key: "091f480c-5dd3-4e53-a49b-2a611fad3fd8",
 };
 
 //My array data coin
@@ -19,27 +19,55 @@ fetch(
     return response.json();
   })
   .then((api) => {
-    var texto = "";
     // Get 20 coins and symbols
     for (let i = 0; i < 20; i++) {
       //Show API information
       data.push(api.data[i]);
-
-      //taking the historical date of each one
-      var date = new Date(data[i].first_historical_data);
-
-      texto += `
-          <div class="media">
-              <img src="coin.jpg" class="align-self-center mr-3" alt="coin">
-              <div class="media-body">
-              <h5 class="mt-2">${data[i].name}</h5>
-              <p>${data[i].symbol} <br> ${date.toDateString()} </p>
-              </div>
-          </div>
-                    `;
-      document.getElementById("coins").innerHTML = texto;
     }
+    coinList(data);
   })
   .catch((error) => {
     console.error(error.message);
   });
+
+//Função buscar no Array data
+function busca() {
+  var texto = document.querySelector("input").value;
+  if (!texto) {
+    console.log("Nenhuma moeda encontrada");
+    coinList(data);
+  } else {
+    const newData = new Array(
+      data.find((coin) => {
+        return coin.name.toLowerCase().includes(texto);
+      })
+    );
+    coinList(newData);
+  }
+}
+
+//Listar em tela
+function coinList(data) {
+  if (!data[0]) {
+    document.getElementById("coins").innerHTML = "Nenhuma moeda encontrada";
+  } else {
+    console.log(data);
+    var texto = "";
+    for (let i = 0; i < data.length; i++) {
+      //taking the historical date of each one
+      var date = new Date(data[i].first_historical_data);
+
+      texto += `
+        <div class="media">
+            <img src="coin.jpg" class="align-self-center mr-3" alt="coin">
+            <div class="media-body">
+            <h5 class="mt-2">${data[i].name}</h5>
+            <p>${data[i].symbol} <br> ${date.toDateString()} </p>
+            </div>
+        </div>
+                  `;
+      document.getElementById("coins").innerHTML = texto;
+    }
+    console.log(data);
+  }
+}
